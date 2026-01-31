8a209e266a8f02f374fec914a9cab0b61fbd2d1c
albertobejaranor125 <albertobejaranor@gmail.com>
2026-01-31 17:12:06 +0100

    Update CinematicUIManager.cs

--- Assets/Scripts/UI/CinematicUIManager.cs
+++ Assets/Scripts/UI/CinematicUIManager.cs
@@ -22,31 +22,16 @@ public class CinematicUIManager : MonoBehaviour
     void Update()
     {
         timerImage += Time.deltaTime;
-        if(timerImage >= imagePerSecond && !_canTween)
+        if(timerImage >= imagePerSecond)
         {
-            _canTween = true;
-            if (imageCount == 0)
+            while (imageCount < imageList.Length && !_canTween)
             {
-                Debug.Log("Se ha movido la primera imagen");
-                var rect = imageList[imageCount].rectTransform;
-                rect.DOMoveX(rect.position.x + 1930, 1).Play();
-                timerImage = 0f;
-            }
-            if(imageCount == 1)
-            {
-                Debug.Log("Se ha movido la segunda imagen");
-                var rect = imageList[imageCount].rectTransform;
-                rect.DOMoveY(rect.position.y + 1930, 1).Play();
-                timerImage = 0f;
-            }
-            if(imageCount == 2)
-            {
-                Debug.Log("Se ha movido la tercera imagen");
+                _canTween = true;
                 var rect = imageList[imageCount].rectTransform;
                 rect.DOMoveX(rect.position.x - 1930, 1).Play();
                 timerImage = 0f;
+                imageCount++;
             }
-            imageCount++;
             _canTween = false;
         }
     }
