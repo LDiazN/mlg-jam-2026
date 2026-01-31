a943bb62b322869badfca4136521725d3cc0fdce
albertobejaranor125 <albertobejaranor@gmail.com>
2026-01-31 13:08:44 +0100

    ImplementJam

--- Assets/Scripts/UI/ImageListUIManager.cs
+++ Assets/Scripts/UI/ImageListUIManager.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ImageListUIManager : MonoBehaviour
 {
+    [SerializeField] private Image[] imageList;
+    private int imageCount;
+    private Image imageShow;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        imageCount = 0;
+        imageShow = imageList[imageCount];
     }
 
     // Update is called once per frame
