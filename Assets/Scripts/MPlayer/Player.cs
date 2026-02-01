124c446e0b7fdcb85211fd6d722eb2eb0c5f14ad
Nicolas M. Pardo <nicolasm@nikodermus.media>
2026-02-01 13:14:46 +0100

    Add Dragon assets and implement Dragon rage commands

--- Assets/Scripts/MPlayer/Player.cs
+++ Assets/Scripts/MPlayer/Player.cs
@@ -1,6 +1,7 @@
 using Control;
 using Scenes.WorldGenerator;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MPlayer
 {
@@ -14,12 +15,23 @@ namespace MPlayer
         public PlayerType type;
         [SerializeField] private PlayerMovement _movement;
         public PlayerMovement Movement => _movement;
+        public Color rageColor = Color.magenta;
+        public Sprite bar;
+        public Color _originalColor;
+        private SpriteRenderer _spriteRenderer;
 
         #endregion
 
         private void Awake()
         {
             _movement = GetComponent<PlayerMovement>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        
+        
+        private void Start()
+        {
+            _originalColor = _spriteRenderer.color;
         }
 
         public void SetItem(
@@ -29,7 +41,39 @@ namespace MPlayer
             item = newItem;
             EventsChannel.PlayerItemUpdate(this, old, newItem);
         }
+        
+        private void OnEnable()
+        {
+            var channel = EventsChannel.Get();
+            if (!channel)
+                return;
+
+            channel.OnDragonRageStarted += OnDragonRageStarted;
+            channel.OnDragonRageFinished += OnDragonRageFinished;
+        }
+
+        private void OnDisable()
+        {
+            var channel = EventsChannel.Get();
+            if (!channel)
+                return;
+
+            channel.OnDragonRageStarted -= OnDragonRageStarted;
+            channel.OnDragonRageFinished -= OnDragonRageFinished;
+        }
+        
+        private void OnDragonRageFinished()
+        {
+            _spriteRenderer.color = _originalColor;
+        }
+
+        private void OnDragonRageStarted()
+        {
+            _spriteRenderer.color = rageColor;
+        }
     }
+    
+    
 
 
     public enum Item
